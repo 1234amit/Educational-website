fd8c535c661062de685809482734cd8a502d2ea8
@1234amit <cseiubatgoswami@gmail.com>
2022-07-03 13:50:04 +0600

    six commit

--- src/components/Teacher.js
+++ src/components/Teacher.js
@@ -11,7 +11,7 @@ const Teacher = () => {
             <div className='description'>
                 <p>We only hire the best teachers to lead PrepScholar Classes. Each of our teachers has scored in the 99th percentile on the test. They've also all attended top colleges. But unlike in other test prep companies, that's not all we look for. We also make sure that everyone on our team is a passionate, talented educator. They don't just know the test; they know how to connect with you, explain complicated ideas, and be patient and motivating guides for you throughout your test prep.</p>
 
-                <p>We only hire the best teachers to lead PrepScholar Classes. Each of our teachers has scored in the 99th percentile on the test. They've also all attended top colleges. But unlike in other test prep companies, that's not all we look for. We also make sure that everyone on our team is a passionate, talented educator. They don't just know the test; they know how to connect with you, explain complicated ideas, and be patient and motivating guides for you throughout your test prep.</p>
+                <p>Since graduating Summa Cum Laude from Harvard University, Eric has been pursuing his PhD from Cambridge University. Between high school and college, Eric founded his own test prep companies, and his passion for teaching students to ace the SAT and ACT hasn't waned since.</p>
             </div>
             
             <a href="#"><button className="btn">learn more</button></a>
