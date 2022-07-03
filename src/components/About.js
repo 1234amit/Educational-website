aa8a2445fc692bbfdd84d3a9ee0a8e557c39d869
@1234amit <cseiubatgoswami@gmail.com>
2022-07-03 13:08:22 +0600

    five commit

--- src/components/About.js
+++ src/components/About.js
@@ -6,20 +6,20 @@ const About = () => {
   return (
 
     <div>
-        <section className="about" id="about">
+        <section className="aboutus" id="about">
+          <h1 className="heading">About Us</h1>
+            <div className="about">
+              <div className="image">
+                  <img src={img1} alt="" />
+              </div>
 
-            <h1 className="heading">About us</h1>
+              <div className="content">
+                  <h3>why choose us?</h3>
+                  <p>Academic Earth is a collection of free online college courses from some of the world’s leading universities and colleges. On its site you’ll be able to find specific courses in many subjects as well as search by university. It collects playlists and video groups to help you learn from many different services, too.</p>
+                  <p>Academic Earth is a smart search tool and a good place to start your journey. It collects many free courses from around the world, so you may click on a lesson plan and be taken to another partner on our list, such as MIT OpenCourseWare.</p>
 
-            <div className="image">
-                <img src={img1} alt="" />
-            </div>
-
-            <div className="content">
-                <h3>why choose us?</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aperiam quaerat dolor voluptas nobis voluptates illum? Inventore voluptas dolore voluptates.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex delectus ducimus dignissimos pariatur. Amet sed distinctio earum dolorum nulla, in obcaecati aliquid modi quos magni ducimus culpa nobis laudantium incidunt.</p>
-
-                <a href="#"><button className="btn">learn more</button></a>
+                  <a href="#"><button className="btn">learn more</button></a>
+              </div>
             </div>
 
         </section>
